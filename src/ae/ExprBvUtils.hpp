5197bd7b4ee5892d947d7b147095147031e930d0
Anna Vasenina <avasenina97@gmail.com>
2022-08-24 15:27:56 +0300

    [fix] add header

---
+++ src/ae/ExprBvUtils.hpp
@@ -0,0 +1,16 @@
+#ifndef EXPRBVSIMPL__HPP__
+#define EXPRBVSIMPL__HPP__
+#include "ufo/Smt/EZ3.hh"
+#include "ufo/ExprBv.hh"
+
+namespace ufo
+{
+  unsigned getBvSize(Expr exp);
+  Expr rewriteSignedCmp(Expr exp);
+  void getSignedCmps (Expr a, ExprVector &scmps);
+//  Expr rewriteDivisible(Expr exp);
+//  Expr rewriteRem(Expr exp);
+  Expr reBuildBvNegCmp(Expr fla, Expr lhs, Expr rhs);
+}
+#endif
+
