0592fe66da091d1a7bc8e91e33bb78d7576e8b06
Anna Vasenina <avasenina97@gmail.com>
2022-10-03 17:40:44 +0300

    codestyle fixes

--- src/ae/ExprBvUtils.hpp
+++ src/ae/ExprBvUtils.hpp
@@ -5,20 +5,19 @@
 
 namespace ufo
 {
-  struct bvMultCoef{
+  struct bvMultCoef
+  {
     int coef;
     bool overflows;
   };
 
   unsigned getBvSize(Expr exp);
   Expr rewriteSignedCmp(Expr exp);
-  void getSignedCmps (Expr a, ExprVector &scmps);
-//  Expr rewriteDivisible(Expr exp);
-//  Expr rewriteRem(Expr exp);
+  void getSignedCmps(Expr a, ExprVector &scmps);
   Expr reBuildBvNegCmp(Expr fla, Expr lhs, Expr rhs);
 
   Expr bvAdditiveInverse(Expr e);
-  void getBaddTerm (Expr a, ExprVector &terms);
+  void getBaddTerm(Expr a, ExprVector &terms);
   Expr bvReBuildCmp(Expr exp, Expr lhs, Expr rhs);
   Expr bvFlipCmp(Expr fla, Expr lhs, Expr rhs);
   bool isBmulVar(Expr e, Expr var);
@@ -27,24 +26,22 @@ namespace ufo
   bool isBdivVar(Expr e, Expr var);
   bool isBmulBdivVar(Expr e, Expr var);
   bool isBvArith(Expr e);
-  void getBvMultVars(Expr e, Expr var, ExprVector& outs);
-  bvMultCoef oveflowChecker(ExprVector& adds, Expr var);
-  bool bvTrySquashCoefs(ExprVector& adds, Expr var);
-  
-  void buleToBult(Expr e, ZSolver<EZ3>::Model& m, ExprSet& out);
-  void bultToBule(Expr e, ZSolver<EZ3>::Model& m, ExprSet& out);
-  void bugeToBugt(Expr e, ZSolver<EZ3>::Model& m, ExprSet& out);
-  void bugtToBuge(Expr e, ZSolver<EZ3>::Model& m, ExprSet& out);
-  Expr rewriteBurem (Expr exp);
-  
-  template<typename Range> static Expr mkbadd(Range& terms){ 
+  void getBvMultVars(Expr e, Expr var, ExprVector &outs);
+  bvMultCoef oveflowChecker(ExprVector &adds, Expr var);
+  bool bvTrySquashCoefs(ExprVector &adds, Expr var);
+
+  void buleToBult(Expr e, ZSolver<EZ3>::Model &m, ExprSet &out);
+  void bultToBule(Expr e, ZSolver<EZ3>::Model &m, ExprSet &out);
+  void bugeToBugt(Expr e, ZSolver<EZ3>::Model &m, ExprSet &out);
+  void bugtToBuge(Expr e, ZSolver<EZ3>::Model &m, ExprSet &out);
+  Expr rewriteBurem(Expr exp);
+
+  template <typename Range>
+  static Expr mkbadd(Range &terms)
+  {
     assert(terms.size() > 0);
-    return
-      (terms.size() == 1) ? *terms.begin() :
-      mknary<BADD>(terms);
+    return (terms.size() == 1) ? *terms.begin() : mknary<BADD>(terms);
   }
 
-
-}
+} // namespace ufo
 #endif
-
